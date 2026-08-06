6e78c7a1d71d919f0a66b30c122624908d574f69
paramsingh <parampreet.digisoft@gmail.com>
2026-08-06 11:55:04 +0530

    commit for changing URL's

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.africahealthintelligence.ai",
-  subscriptionUrl:'https://africahealthintelligence.ai',
+  apiUrl: "https://api.veridianclimatepulse.org",
+  subscriptionUrl:'https://veridianclimatepulse.org',
   googleClientId: '419053098195-ink9t3quhqbfc0li446rn1qf9tci4hm3.apps.googleusercontent.com',
   facebookAppId: '1784178838961034',
   stripePublicKey: 'pk_test_VhPtypJ0veVOYXRHer1kazHJ',
